c91957ef43b7e72891003debb9b2b307808d3f45
edems <adamtorok@sch.bme.hu>
2018-05-18 19:02:00 +0200

    beauty

--- shaders/geometry.glsl
+++ shaders/geometry.glsl
@@ -19,14 +19,14 @@ void main (void) {
     float t3 = GetT(t2, gl_in[2].gl_Position.xy, gl_in[3].gl_Position.xy);
 
     for(float t = t1; t < t2; t += ((t2 - t1) / 10.0f)) {
-        vec2 A1 = (t1-t) / (t1-t0) * gl_in[0].gl_Position.xy + (t-t0) / (t1-t0) * gl_in[1].gl_Position.xy;
-        vec2 A2 = (t2-t) / (t2-t1) * gl_in[1].gl_Position.xy + (t-t1) / (t2-t1) * gl_in[2].gl_Position.xy;
-        vec2 A3 = (t3-t) / (t3-t2) * gl_in[2].gl_Position.xy + (t-t2) / (t3-t2) * gl_in[3].gl_Position.xy;
+        vec2 A1 = (t1 - t) / (t1 - t0) * gl_in[0].gl_Position.xy + (t - t0) / (t1 - t0) * gl_in[1].gl_Position.xy;
+        vec2 A2 = (t2 - t) / (t2 - t1) * gl_in[1].gl_Position.xy + (t - t1) / (t2 - t1) * gl_in[2].gl_Position.xy;
+        vec2 A3 = (t3 - t) / (t3 - t2) * gl_in[2].gl_Position.xy + (t - t2) / (t3 - t2) * gl_in[3].gl_Position.xy;
 
-        vec2 B1 = (t2-t) / (t2-t0) * A1 + (t-t0) / (t2-t0) * A2;
-        vec2 B2 = (t3-t) / (t3-t1) * A2 + (t-t1) / (t3-t1) * A3;
+        vec2 B1 = (t2 - t) / (t2 - t0) * A1 + (t - t0) / (t2 - t0) * A2;
+        vec2 B2 = (t3 - t) / (t3 - t1) * A2 + (t - t1) / (t3 - t1) * A3;
 
-        vec2 C = (t2-t) / (t2-t1) * B1 + (t-t1) / (t2-t1) * B2;
+        vec2 C = (t2 - t) / (t2 - t1) * B1 + (t - t1) / (t2 - t1) * B2;
 
         gl_Position = vec4(C, 0.0f, 1.0f);
         EmitVertex();
